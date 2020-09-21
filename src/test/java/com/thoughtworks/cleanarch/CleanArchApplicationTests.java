ea04d37976d568fccaf5c1781e2adebaae92b6a6
sdp <403213485@qq.com>
2020-09-21 18:03:02 +0800

    should_return_inputStr_when_given_input

--- src/test/java/com/thoughtworks/cleanarch/CleanArchApplicationTests.java
+++ src/test/java/com/thoughtworks/cleanarch/CleanArchApplicationTests.java
@@ -3,11 +3,19 @@ package com.thoughtworks.cleanarch;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class CleanArchApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void should_return_inputStr_when_given_input() {
+		String inputArgs = "-l true -p 8080 -d /usr/logs";
+
+		Args args = new Args(inputArgs);
+
+		assertEquals("[Arg{flag='l', value=true}, Arg{flag='p', value=8080}" +
+				", Arg{flag='d', value=/usr/logs}]",args.getArgList().toString());
 	}
 
 }
